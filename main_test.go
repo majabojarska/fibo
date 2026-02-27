43d3bebe39110d0a793d33c36b95a01cde11e944
Maja Bojarska <majabojarska98@gmail.com>
2026-02-27 03:20:04 +0100

    e2e: healthchecks

--- main_test.go
+++ main_test.go
@@ -102,3 +102,60 @@ func TestGetFibonacci(t *testing.T) {
 		})
 	}
 }
+
+// TestHealthchecks tests the /readyz and /livez GET endpoints.
+func TestHealthchecks(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := setupRouter()
+
+	tests := []struct {
+		name           string
+		url            string
+		method         string
+		wantStatusCode int
+		wantContains   string
+	}{
+		{
+			name:           "GET readyz",
+			url:            "/readyz",
+			method:         http.MethodGet,
+			wantStatusCode: http.StatusOK,
+			wantContains:   "",
+		},
+		{
+			name:           "POST readyz",
+			url:            "/readyz",
+			method:         http.MethodPost,
+			wantStatusCode: http.StatusNotFound,
+			wantContains:   "404 page not found",
+		},
+		{
+			name:           "GET livez",
+			method:         http.MethodGet,
+			url:            "/livez",
+			wantStatusCode: http.StatusOK,
+			wantContains:   "",
+		},
+		{
+			name:           "PUT livez",
+			method:         http.MethodPut,
+			url:            "/livez",
+			wantStatusCode: http.StatusNotFound,
+			wantContains:   "404 page not found",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var req *http.Request
+
+			req, _ = http.NewRequest(tt.method, tt.url, nil)
+
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			assert.Equal(t, tt.wantStatusCode, recorder.Code)
+			assert.Equal(t, tt.wantContains, recorder.Body.String())
+		})
+	}
+}
