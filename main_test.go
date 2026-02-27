abf0d235763493780718703aa31c01e989933a33
Maja Bojarska <majabojarska98@gmail.com>
2026-02-27 01:36:40 +0100

    e2e: minimal parametrized tests

--- main_test.go
+++ main_test.go
@@ -1,19 +1,50 @@
 package main
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFibonacci(t *testing.T) {
+func TestGetFibonacci(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	router := setupRouter()
-	recorder := httptest.NewRecorder()
-	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/api/v1/fibonacci/0", nil)
-	router.ServeHTTP(recorder, req)
 
-	assert.Equal(t, http.StatusOK, recorder.Code)
+	tests := []struct {
+		name           string
+		method         string
+		url            string
+		wantStatusCode int
+		wantContains   string
+		wantHeaders    gin.H
+	}{
+		{
+			name:           "Valid fibonacci sequence request (empty)",
+			method:         http.MethodGet,
+			url:            "/api/v1/fibonacci/0",
+			wantStatusCode: http.StatusOK,
+			wantContains:   "[]",
+			wantHeaders: gin.H{
+				http.CanonicalHeaderKey("Content-Type"):      "application/json",
+				http.CanonicalHeaderKey("Transfer-Encoding"): "chunked",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var req *http.Request
+
+			req, _ = http.NewRequest(tt.method, tt.url, nil)
+
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			assert.Equal(t, tt.wantStatusCode, recorder.Code)
+			assert.Contains(t, recorder.Body.String(), tt.wantContains)
+		})
+	}
 }
