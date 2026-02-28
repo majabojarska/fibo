604c8aa9718eadb893f84de092d270f5502bb419
Maja Bojarska <majabojarska98@gmail.com>
2026-02-28 01:47:35 +0100

    setup ginzap

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"time"
+
+	ginZap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	ctrl "github.com/majabojarska/fibo/controller"
 	_ "github.com/majabojarska/fibo/docs" // Swaggo requires this to be imported
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"go.uber.org/zap"
 )
 
 //	@title			Fibo
@@ -28,10 +32,14 @@ const (
 	metricsPathDefault       = "/metrics"
 )
 
-func setupRouter() *gin.Engine {
-	router := gin.Default()
+func setupMiddlewares(router *gin.Engine, logger *zap.Logger) {
 	SetupPromMiddleware(router, metricsListenAddrDefault, metricsPathDefault) // Must be before route setup
 
+	router.Use(ginZap.Ginzap(logger, time.RFC3339, true))
+	router.Use(ginZap.RecoveryWithZap(logger, true))
+}
+
+func setupRoutes(router *gin.Engine) {
 	ctrl := ctrl.NewController()
 
 	groupV1 := router.Group("/api/v1")
@@ -45,15 +53,27 @@ func setupRouter() *gin.Engine {
 	router.GET("/readyz", ctrl.GetReadyz)
 	router.GET("/livez", ctrl.GetLivez)
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
+
+func setupRouter(logger *zap.Logger) *gin.Engine {
+	router := gin.Default()
+
+	setupMiddlewares(router, logger)
+	setupRoutes(router)
 
 	return router
 }
 
 func main() {
-	router := setupRouter()
-
-	err := router.Run(apiListenAddrDefault)
+	logger, err := zap.NewDevelopment() // TODO: Make this gated by FIBO_DEBUG once viper is in
 	if err != nil {
 		panic(err)
+	}
+
+	router := setupRouter(logger)
+
+	err = router.Run(apiListenAddrDefault)
+	if err != nil {
+		logger.Sugar().Error(err)
 	}
 }
