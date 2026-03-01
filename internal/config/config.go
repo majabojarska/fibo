96113a93d18184174f62c7b816355f157b408e6f
Maja Bojarska <majabojarska98@gmail.com>
2026-03-01 03:10:56 +0100

    define new config structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,37 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	Api     ApiConfig
+	Docs    DocsConfig
+	Logging LoggingConfig
+	Metrics MetricsConfig
+	Pprof   PprofConfig
+}
+
+type ApiConfig struct {
+	Addr string
+}
+
+type DocsConfig struct {
+	Enabled bool
+}
+
+type LoggingConfig struct {
+	// https://pkg.go.dev/go.uber.org/zap#AtomicLevel.UnmarshalText
+	Level string
+}
+
+type MetricsConfig struct {
+	Enabled bool
+	Addr    string
+	Path    string
+}
+
+type PprofConfig struct {
+	Enabled bool
+}
+
 func setDefaults() {
 	viper.SetDefault("api.addr", ":8080")
 	viper.SetDefault("docs.enabled", true)
